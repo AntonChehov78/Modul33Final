47f6857f3358685bad6c4f6b192a35fd27706e5a
AntonBaranov78 <spapjshik@yandex.ru>
2023-06-01 11:01:17 +0300

    Финальное задание модуль 33

---
+++ settings.py
@@ -0,0 +1,7 @@
+
+valid_email = 'nataliya1175@mail.ru'
+valid_password = 'wPC-pVW-MK5-mvZ'
+invalid_email = 'none@mail.ru'
+invalid_password = 'password'
+correct_phone = '+79536101999'
+incorrect_phone = '+79998887766'
